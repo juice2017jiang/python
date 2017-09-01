af941050d9e044664b071e2b85461ec706c5fe63
JTT <JTT>
2017-09-01 15:44:01 +0800

    f1aa

---
+++ 728.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+import unittest
+def add(a,b):
+        c=a+b
+        return c
+
+#class Test(unittest.TestCase):
+def test():
+    c=add(1,4)
+    self.assertEqual(c,6)
+        
+    return ("pass")
+        
+   
+
